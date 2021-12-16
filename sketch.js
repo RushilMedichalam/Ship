3a8003a585ec745dd96aa9493691ccd0c60c5f14
RushilMedichalam <96029135+RushilMedichalam@users.noreply.github.com>
2021-12-15 19:09:59 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+function setup() {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+
+
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    background("red");
+    
+  }
+
+  
+  drawSprites();
+}
+
